a9735b0f53e86349d36cc98d1852f40bc6be2d54
Robert van den Heuvel <robert_vd_heuvel@hotmail.com>
2019-04-04 16:52:52 +0200

    status urenformapi - Robert

--- src/main/java/com/Mijnqien/api/UrenFormAPI.java
+++ src/main/java/com/Mijnqien/api/UrenFormAPI.java
@@ -1,17 +1,27 @@
 package com.Mijnqien.api;
 
+import java.util.Optional;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.Mijnqien.Exceptions.UrenFormNotFoundException;
+import com.Mijnqien.Ondersteunend.EmailServer;
+import com.Mijnqien.Ondersteunend.ReadProperties;
 import com.Mijnqien.Trainee.UrenForm;
 import com.Mijnqien.service.UrenFormService;
+import com.Mijnqien.Trainee.Stat;
 
 @Component
 @Path("urenform")
@@ -21,18 +31,84 @@ public class UrenFormAPI {
 	@Autowired
 	UrenFormService urenFormService;
 	
+	@Autowired
+	EmailServer emailserver; 
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	
-	public Iterable<UrenForm> getUrenForms(){
+
+	public Iterable<UrenForm> getUrenForms() {
 		Iterable<UrenForm> urenForms = urenFormService.findAlleUrenForms();
 		return urenForms;
 	}
-	
+
 	@POST
 	public Response inDatabaseStoppen(UrenForm urenForm) {
 		UrenForm urenForm2 = urenFormService.saveUrenForm(urenForm);
 		return Response.ok(urenForm2).build();
 	}
+
+	@PUT
+	@Path("/verzend/{FormID}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response verzendUrenForm(@PathParam("FormID") long FormID) {
+		try {
+			UrenForm urenForm = urenFormService.findById(FormID);
+			if(urenForm.getStat() == Stat.INAFWACHTING) {
+				urenForm.setStat(Stat.INGEDIEND);
+				ReadProperties.readConfig();
+				urenForm = urenFormService.saveUrenForm(urenForm);
+				emailserver.send(ReadProperties.setAdminMail,"Gebruikersnaam" + "urenForm " + urenForm.getMaand().getMonth().toString() + " " + urenForm.getMaand().getYear(),"");
+				return Response.status(Status.ACCEPTED).build();
+			}
+			return Response.status(Status.BAD_REQUEST).build();			
+		}catch(UrenFormNotFoundException e){
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		
+	}
 	
+	@PUT
+	@Path("/verzend/{FormID}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response afkeurUrenForm(@PathParam("FormID") long FormID) {
+		try {
+			UrenForm urenForm = urenFormService.findById(FormID);
+			if(urenForm.getStat() == Stat.INGEDIEND) {
+				urenForm.setStat(Stat.WIJZIGEN);
+				ReadProperties.readConfig();
+				urenForm = urenFormService.saveUrenForm(urenForm);
+				emailserver.send(ReadProperties.setAdminMail,"Gebruikersnaam" + "urenForm " + urenForm.getMaand().getMonth().toString() + " " + urenForm.getMaand().getYear(),"");
+				return Response.status(Status.ACCEPTED).build();
+			}
+			return Response.status(Status.BAD_REQUEST).build();			
+		}catch(UrenFormNotFoundException e){
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		
+	}
+	
+	@PUT
+	@Path("/verzend/{FormID}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response goedkeurUrenForm(@PathParam("FormID") long FormID) {
+		try {
+			UrenForm urenForm = urenFormService.findById(FormID);
+			if(urenForm.getStat() == Stat.INGEDIEND) {
+				urenForm.setStat(Stat.GOEDGEKEURD);
+				ReadProperties.readConfig();
+				urenForm = urenFormService.saveUrenForm(urenForm);
+				emailserver.send(ReadProperties.setAdminMail,"Gebruikersnaam" + "urenForm " + urenForm.getMaand().getMonth().toString() + " " + urenForm.getMaand().getYear(),"");
+				return Response.status(Status.ACCEPTED).build();
+			}
+			return Response.status(Status.BAD_REQUEST).build();			
+		}catch(UrenFormNotFoundException e){
+			return Response.status(Status.NOT_FOUND).build();
+		}
+		
+	}
+
 }
