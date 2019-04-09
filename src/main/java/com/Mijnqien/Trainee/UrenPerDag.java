8d28f04248d0f7cbf9154f0bddef0b84b5342b3f
JDonker <jasperdonker@gmail.com>
2019-04-09 13:17:44 +0200

    Jasper: security 2

--- src/main/java/com/Mijnqien/Trainee/UrenPerDag.java
+++ src/main/java/com/Mijnqien/Trainee/UrenPerDag.java
@@ -28,6 +28,28 @@ public class UrenPerDag {
 	int ziek;
 	int verlof;
 	String verklaringOverig = "";
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UrenPerDag other = (UrenPerDag) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
 	int overig;
 	String datum;
 
